ed51d230e72b8ac7aa38493bfdf89292ad3837b3
miklegonza <gonzalez.michael1803@gmail.com>
2024-07-12 14:49:56 -0500

    :tada: First commit

---
+++ src/main/java/live/michaelgonzalez/lerbackend/LerBackendApplication.java
@@ -0,0 +1,13 @@
+package live.michaelgonzalez.lerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/michaelgonzalez/lerbackend/LerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package live.michaelgonzalez.lerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
